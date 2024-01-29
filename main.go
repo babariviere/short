c7be765c236c66db34242c3cf0a13b40aa4bf461
Bastien Riviere <me@babariviere.com>
2024-01-29 19:53:15 +0100

    feat: get postgresql URL from env

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/babariviere/short/internal/api"
 	"github.com/babariviere/short/internal/db"
@@ -12,7 +13,13 @@ import (
 )
 
 func main() {
-	psql, err := pgx.Connect(context.Background(), "postgres://short:short@localhost:5432/short")
+	// This is rude but at least we can configure the database URL securely.
+	psqlUrl := os.Getenv("DATABASE_URL")
+	if psqlUrl == "" {
+		log.Fatalln("DATABASE_URL is not configured.")
+	}
+
+	psql, err := pgx.Connect(context.Background(), psqlUrl)
 	if err != nil {
 		log.Fatalf("cannot connect to database: %v", err)
 	}
