324a5b31e090e474e4a489025a5afa9c4b1d85cd
Bastien Riviere <me@babariviere.com>
2024-02-09 19:36:05 +0100

    chore: cleanup handler

--- internal/api/handler.go
+++ internal/api/handler.go
@@ -27,10 +27,11 @@ func NewHandler(serverUrl string, queries *db.Queries) *handler {
 
 func (h *handler) RedirectLongURL(ctx context.Context, params oas.RedirectLongURLParams) (oas.RedirectLongURLRes, error) {
 	res, err := h.queries.GetURLByHash(ctx, params.Hash)
-	if err != nil {
-		if errors.Is(err, pgx.ErrNoRows) {
-			return &oas.RedirectLongURLNotFound{}, nil
-		}
+
+	switch {
+	case errors.Is(err, pgx.ErrNoRows):
+		return &oas.RedirectLongURLNotFound{}, nil
+	case err != nil:
 		return nil, fmt.Errorf("unable to fetch URL by hash: %w", err)
 	}
 
